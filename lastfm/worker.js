97c000a5fe67854dd10cde8ece1e79f7a228373f
MathiasDPX <mathias@dupeux.net>
2026-03-10 17:52:03 +0100

    fix: rename env variables

--- lastfm/worker.js
+++ lastfm/worker.js
@@ -1,22 +1,32 @@
+function makeResponse(data, status = 200) {
+    return new Response(JSON.stringify(data), {
+        status,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
 export default {
     async fetch(request, env, ctx) {
-        const API_KEY = env.API_KEY;
-        const LASTFM_USERNAME = env.LASTFM_USERNAME;
+        const API_KEY = env.LASTFM_API_KEY;
+        const LASTFM_USERNAME = env.LAST_FM_USERNAME;
 
         const res = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LASTFM_USERNAME}&api_key=${API_KEY}&format=json&limit=1`);
         if (!res.ok) {
-            return new Response("Failed to fetch last.fm api", { status: 500 });
+            return makeResponse({"error": true, "message": "failed to fetch last.fm data"}, 500);
         }
 
         const result = await res.json();
         const tracks = result['recenttracks']['track'];
 
-        let response = new Response(JSON.stringify({"playing": false}), { status: 200 });
+        let response = makeResponse({"playing": false});
         tracks.forEach((track) => {
             if (track?.['@attr']?.['nowplaying'] !== "true") {
                 return response
             }
             const data = {
+                "playing": true,
                 "artist": track?.['artist']?.['#text'],
                 "name": track?.['name'],
                 "album": track?.['album']?.['#text'],
@@ -27,12 +37,7 @@ export default {
                 }, {})
             };
 
-            response = new Response(JSON.stringify(data), {
-                status: 200,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+            response = makeResponse(data);
         });
 
         return response;
