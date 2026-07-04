9c477872aa6853c7431206dfccfbd1b50edf390b
MathiasDPX <mathias@dupeux.net>
2026-07-04 21:10:54 +0200

    fix: bump version

---
+++ gojs/README.md
@@ -0,0 +1,3 @@
+Replace the domain check with `1` to tell Go.JS that the current URL is part of `go.js`
+
+RC4 Key: `0x77`
--- gojs/worker.js
+++ gojs/worker.js
@@ -1,14 +1,21 @@
 export default {
     async fetch(request) {
+        const { searchParams } = new URL(request.url)
+        let version = searchParams.get('v') || "latest"
+
         // Fetch original GoJS
-        const response = await fetch("https://gojs.net/latest/release/go.js");
+        const response = await fetch(`https://gojs.net/${version}/release/go.js`);
         if (!response.ok) {
             return new Response("Failed to fetch go.js", { status: 500 });
         }
 
-        // Patch domain check
         let content = await response.text();
-        content = content.replace(/const a=i\[n\("[0-9a-fA-F]+"\)\]\(n\("[0-9a-fA-F]+"\)\);/g, `const a=1;`);
+
+        // Patch domain check
+        content = content.replace(
+            /[a-z]\[[a-zA-Z]{1,2}\("73a612b6fb191d"\)\]\([a-zA-Z]{1,2}\("7da71ca0ad381e90"\)\)/g,
+            "1"
+        );
 
         return new Response(content, {
             status: 200,
